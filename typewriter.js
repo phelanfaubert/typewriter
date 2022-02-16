0523d88746a1aaa06877a815e3eae7bfecd37fcf
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-16 04:51:58 +0000

    Added comments to code

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,9 @@
 let sentence = "hello there from lighthouse labs";
+// fixes the bug, wanting to start a new line after sentence writes out
 sentence += "\n";
+// setting timer to a variable so we can +/- 
 let delay = 1000;
+//looping the given sentence
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
